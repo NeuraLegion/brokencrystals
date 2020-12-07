ed1c6f5e80de7fb794f5cf8158df62aa88acebfa
Gil Messerman <gilm@sogoservices.com>
2020-12-07 17:43:18 +0200

    crystal images for dynamic loading

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as jwt from 'jwt-simple';
 
 @Injectable()
 export class AuthService {
     
+    async validateToken(token: string) : Promise<boolean> {
+        // jwt.decode('a','a', true, jwt.)
+        
+        return null;
+    }
+
+
+    async createToken(payload: unknown): Promise<string> {
+        // jwt.encode()
+        return null;
+    }
 }
