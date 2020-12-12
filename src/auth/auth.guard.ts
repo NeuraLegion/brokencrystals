f10e48255a539be625d12d2098ceaf7944beaa46
Gil Messerman <gilm@sogoservices.com>
2020-12-12 19:28:59 +0200

    auth guard start

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard {
+  async canActivate(context) {
+    const request = context.switchToHttp().getRequest();
+    return true;//validateRequest(request);
+  }
+}
