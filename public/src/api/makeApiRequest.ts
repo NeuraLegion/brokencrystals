a29d95f0f7bdf5342f04556c176dbe0cbfe48165
Bekto Dedic <bektodedic98@gmail.com>
2021-04-19 11:00:38 +0200

    feat(auth): add token expiration time (#28)

--- public/src/api/makeApiRequest.ts
+++ public/src/api/makeApiRequest.ts
@@ -7,12 +7,17 @@ export function makeApiRequest<T>(
   const config: AxiosRequestConfig =
     typeof urlOrConfig === 'string' ? { url: urlOrConfig } : urlOrConfig;
 
-  return httpClient.request(config).then((response) => {
-    if (response) {
+  return httpClient
+    .request(config)
+    .then((response) => {
       const token = response.headers.authorization;
       token && sessionStorage.setItem('token', token);
-
       return response.data;
-    }
-  });
+    })
+    .catch((error) => {
+      if (error.response.status === 401) {
+        sessionStorage.removeItem('token');
+        window.location.reload();
+      }
+    });
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -115,7 +115,10 @@ export class AuthController {
     res.header(
       'authorization',
       await this.authService.createToken(
-        { user: profile.email },
+        { 
+          user: profile.email,
+          exp:  90 + Math.floor(Date.now() / 1000)
+        },
         JwtProcessorType.RSA,
       ),
     );
