6d244baf0e1a17afd7e5a928fb83c3ed529948ab
Tamir Gershberg <47638346+tamirGer@users.noreply.github.com>
2021-08-17 13:25:31 +0300

    refactor(nav): move repetetive and unreadable html into an object (#67)

--- public/src/pages/main/Header/Nav.tsx
+++ public/src/pages/main/Header/Nav.tsx
@@ -1,98 +1,105 @@
 import React from 'react';
+interface MenuItem {
+  name: string;
+  path: string;
+  newTab: boolean;
+  subItems?: Array<MenuItem>;
+}
+
+const menu: Array<MenuItem> = [
+  { name: 'Home', path: '/', newTab: false },
+  { name: 'Marketplace', path: '/marketplace', newTab: false },
+  {
+    name: 'Gemstones',
+    path: '',
+    newTab: false,
+    subItems: [
+      { name: 'Gemstone 1', path: '', newTab: false },
+      { name: 'Gemstone 2', path: '', newTab: false },
+      { name: 'Gemstone 3', path: '', newTab: false },
+      { name: 'Gemstone 4', path: '', newTab: false }
+    ]
+  },
+  {
+    name: 'Healing',
+    path: '',
+    newTab: false,
+    subItems: [
+      { name: 'Healing 1', path: '', newTab: false },
+      { name: 'Healing 2', path: '', newTab: false },
+      { name: 'Healing 3', path: '', newTab: false },
+      { name: 'Healing 4', path: '', newTab: false }
+    ]
+  },
+  {
+    name: 'Jewellery',
+    path: '',
+    newTab: false,
+    subItems: [
+      { name: 'Jewellery 1', path: '', newTab: false },
+      { name: 'Jewellery 2', path: '', newTab: false },
+      { name: 'Jewellery 3', path: '', newTab: false },
+      { name: 'Jewellery 4', path: '', newTab: false }
+    ]
+  },
+  { name: 'Contact', path: '', newTab: false },
+  {
+    name: 'API Schema',
+    path: '',
+    newTab: false,
+    subItems: [
+      {
+        name: 'OpenAPI 3.0 JSON',
+        path: 'https://brokencrystals.com/swagger/json',
+        newTab: true
+      },
+      {
+        name: 'Schema Editor',
+        path: 'https://brokencrystals.com/swagger/static/index.html',
+        newTab: true
+      }
+    ]
+  },
+  {
+    name: 'Vulnerabilities',
+    path:
+      'https://github.com/NeuraLegion/brokencrystals#vulnerabilities-overview',
+    newTab: true
+  }
+];
 
 export const Nav = () => {
   return (
     <nav className="nav-menu d-none d-lg-block">
       <ul>
-        <li className="active">
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/marketplace">Marketplace</a>
-        </li>
-        <li className="drop-down">
-          <a href="">Gemstones</a>
-          <ul>
-            <li>
-              <a href="/">Gemstone 1</a>
-            </li>
-            <li>
-              <a href="/">Gemstone 2</a>
-            </li>
-            <li>
-              <a href="/">Gemstone 3</a>
-            </li>
-            <li>
-              <a href="/">Gemstone 4</a>
-            </li>
-          </ul>
-        </li>
-        <li className="drop-down">
-          <a href="">Healing</a>
-          <ul>
-            <li>
-              <a href="/">Healing 1</a>
-            </li>
-            <li>
-              <a href="/">Healing 2</a>
-            </li>
-            <li>
-              <a href="/">Healing 3</a>
-            </li>
-            <li>
-              <a href="/">Healing 4</a>
-            </li>
-          </ul>
-        </li>
-        <li className="drop-down">
-          <a href="">Jewellery</a>
-          <ul>
-            <li>
-              <a href="/">Jewellery 1</a>
-            </li>
-            <li>
-              <a href="/">Jewellery 2</a>
-            </li>
-            <li>
-              <a href="/">Jewellery 3</a>
-            </li>
-            <li>
-              <a href="/">Jewellery 4</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="/">Contact</a>
-        </li>
-        <li className="drop-down">
-          <a href="">API Schema</a>
-          <ul>
-            <li>
-              <a href="https://brokencrystals.com/swagger/json" target="_blank">
-                OpenAPI 3.0 JSON
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://brokencrystals.com/swagger/static/index.html"
-                target="_blank"
-              >
-                API Reference
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a
-            href="https://github.com/NeuraLegion/brokencrystals#vulnerabilities-overview"
-            target="_blank"
+        {menu.map((item, i) => (
+          <li
+            className={`${item.subItems && 'drop-down'} ${
+              window.location.pathname == item.path && 'active'
+            }`}
+            key={i}
           >
-            Vulnerabilities
-          </a>
-        </li>
+            <a href={item.path} target={item.newTab ? '_blank' : undefined}>
+              {item.name}
+            </a>
+            {item.subItems && (
+              <ul>
+                {item.subItems.map((item, i) => (
+                  <li key={i}>
+                    <a
+                      href={item.path}
+                      target={item.newTab ? '_blank' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
       </ul>
     </nav>
   );
 };
-
 export default Nav;
