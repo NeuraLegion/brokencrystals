1d39b245cd557588a93c5e964ea2e70f2d9eb9cf
Tamir Gershberg <47638346+tamirGer@users.noreply.github.com>
2021-10-04 15:29:54 +0300

    Update auth.controller.swagger.desc.ts

--- src/auth/auth.controller.swagger.desc.ts
+++ src/auth/auth.controller.swagger.desc.ts
@@ -11,11 +11,11 @@ export const SWAGGER_DESC_callOIDCClient = `
 `;
 
 export const SWAGGER_DESC_validateWithKIDSqlJwt = `
-    Validates the JWT header and return secret if the header is valid. Executes SQL query by concatentatin the KID value to the query.
+    Validates the JWT header and returns secret if the header is valid. Executes SQL query by concatenating the KID value with the query.
     In case of None algorithm ignores the signature.
 `;
 export const SWAGGER_DESC_loginWithWeakKeyJwt = `
-    Authenticates user and returns JWT token with HCA256. The key is configurable on server via JWT_SECRET_KEY env variable.
+    Authenticates user and returns JWT token with HCA256. The key is configurable on server via the JWT_SECRET_KEY env variable.
 `;
 export const SWAGGER_DESC_validateWithWeakKeyJwt = `
     Validates the JWT header and return secret if the header is valid. The token validation is done using algorithm in header and key
@@ -23,11 +23,11 @@ export const SWAGGER_DESC_validateWithWeakKeyJwt = `
 `;
 
 export const SWAGGER_DESC_loginWithJKUJwt = `
-    Authenticates user and returns JWT token with HCA256. The key is configurable on server via JWT_SECRET_KEY env variable.
+    Authenticates user and returns JWT token with HCA256. The key is configurable on server via the JWT_SECRET_KEY env variable.
 `;
 
 export const SWAGGER_DESC_validateWithJKUJwt = `
-    Vurenability case: Uses publicly available JSON to check if JWT is properly signed after the Header in JWT is set to point to our JSON and sign the JWT with our private key.
+    Vulnerability case: Uses publicly available JSON to check if JWT is properly signed after the Header in JWT is set to point to our JSON and sign the JWT with our private key.
 `;
 
 export const SWAGGER_DESC_loginWithJWKJwt = `
@@ -35,21 +35,21 @@ export const SWAGGER_DESC_loginWithJWKJwt = `
 `;
 
 export const SWAGGER_DESC_validateWithJWKJwt = `
-    Vurenability case: JWK field JSON in Header has empty values and our private key to sign the JWT
+    Vulrenability case: JWK field JSON in Header has empty values and our private key to sign the JWT
 `;
 
 export const SWAGGER_DESC_loginWithX5CJwt = `
-    Authenticates user and returns JWT token with HCA256. The key is configurable on server via JWT_SECRET_KEY env variable.
+    Authenticates user and returns JWT token with HCA256. The key is configurable on server via the JWT_SECRET_KEY env variable.
 `;
 
 export const SWAGGER_DESC_validateWithX5CJwt = `
-    Vurenability case: The application doesn’t properly check which X5C key is used for signing and when X5C headers are set to our values and signed with our private key, authentication is bypassed.
+    Vulnerability case: The application doesn’t properly check which X5C key is used for signing and when X5C headers are set to our values and signed with our private key, authentication is bypassed.
 `;
 
 export const SWAGGER_DESC_loginWithX5UJwt = `
-    Authenticates user and returns JWT token with HCA256. The key is configurable on server via JWT_SECRET_KEY env variable.
+    Authenticates user and returns JWT token with HCA256. The key is configurable on server via the JWT_SECRET_KEY env variable.
 `;
 
 export const SWAGGER_DESC_validateWithX5UJwt = `
-    Vurenability case: Uses the uploaded certificate to sign the JWT and sets the X5U Header in JWT to point to the uploaded certificate.
+    Vulnerability case: Uses the uploaded certificate to sign the JWT and sets the X5U Header in JWT to point to the uploaded certificate.
 `;
