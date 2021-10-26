e0e9bfd9b589ebc3708e17bdc180b9a6957399ed
Kif <kirillfando@gmail.com>
2021-10-26 10:32:46 +0300

    docs: improved OAS schema

--- src/users/users.controller.swagger.desc.ts
+++ src/users/users.controller.swagger.desc.ts
@@ -2,10 +2,12 @@ export const SWAGGER_DESC_OPTIONS_REQUEST = `List of permitted communication opt
 
 export const SWAGGER_DESC_FIND_USER_BY_EMAIL = `Returns user by email`;
 
+export const SWAGGER_DESC_PHOTO_USER_BY_EMAIL = `Returns user profile photo`;
+
 export const SWAGGER_DESC_LDAP_SEARCH = `Performs LDAP search for user details`;
 
 export const SWAGGER_DESC_CREATE_BASIC_USER = `Creates BASIC user in PostGreSQL db`;
 
-export const SWAGGER_DESC_CREATE_OIDCC_USER = `Creates OIDC user in KeyCloak db`;
+export const SWAGGER_DESC_CREATE_OIDC_USER = `Creates OIDC user in KeyCloak db`;
 
 export const SWAGGER_DESC_UPLOAD_USER_PHOTO = `Uploads user profile photo`;
