825de1b008c01ce09cb77da7a0f9c09b3161f3b2
Kirill <kirillfando@gmail.com>
2022-12-13 18:16:01 +0100

    feat: add UI for file uploading vulnerability (#219)

--- public/src/pages/marketplace/Marketplace.tsx
+++ public/src/pages/marketplace/Marketplace.tsx
@@ -1,6 +1,11 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import { Product } from '../../interfaces/Product';
-import { getProducts, getLatestProducts } from '../../api/httpClient';
+import {
+  getProducts,
+  getLatestProducts,
+  putFile,
+  getFile
+} from '../../api/httpClient';
 import Header from '../main/Header/Header';
 import Testimonials from './Testimonials/Testimonials';
 import ProductView from './ProductView';
@@ -8,9 +13,33 @@ import ProductView from './ProductView';
 interface Props {
   preview: boolean;
 }
+const path = process.env.NODE_ENV === 'production' ? '/home/node/' : '';
 
 export const Marketplace: FC<Props> = (props: Props) => {
   const [products, setProducts] = useState<Array<Product>>([]);
+  const [sendFileResult, setSendFileResult] = useState<string>('');
+  const [fileName, setFileName] = useState<string>('');
+
+  const sendFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const file: File = (e.target.files as FileList)[0];
+    putFile(`${path}${file.name}`, file).then((result) => {
+      setSendFileResult(result);
+      setFileName(file.name);
+    });
+  };
+
+  const onGetFile = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    getFile(`${path}${fileName}`).then((blob) => {
+      const url = window.URL.createObjectURL(new Blob([blob]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', fileName);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  };
 
   useEffect(() => {
     props.preview
@@ -56,8 +85,40 @@ export const Marketplace: FC<Props> = (props: Props) => {
           </div>
         )}
       </section>
-
       <Testimonials preview={props.preview} />
+      <section id="feedback" className="testimonials section-bg">
+        <div className="container" data-aos="fade-up">
+          <div className="section-title">
+            <h2>feedback</h2>
+            <span>Please, upload a feedback: </span>
+            <label htmlFor="feedback-file-input" className="file-input-label">
+              <img
+                src={'assets/img/upload-file.svg'}
+                alt=""
+                className="upload-file-image"
+              />
+            </label>
+            <input
+              id="feedback-file-input"
+              type="file"
+              accept="file/*"
+              style={{ display: 'none' }}
+              onChange={sendFile}
+            />
+            {sendFileResult.length > 0 && (
+              <>
+                <div className="warning-text">{sendFileResult}</div>
+                <div>
+                  You can reach your file{' '}
+                  <a href="#" onClick={onGetFile}>
+                    here
+                  </a>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
