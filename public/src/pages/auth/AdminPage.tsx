455c74065ce21d4571c6c9175953f47c0af803e5
Tamir Gershberg <tamir.ger@gmail.com>
2023-02-03 19:01:29 +0200

    revert unrelated change

--- public/src/pages/auth/AdminPage.tsx
+++ public/src/pages/auth/AdminPage.tsx
@@ -27,9 +27,7 @@ export const AdminPage: FC = () => {
       {isAdmin ? (
         <>
           <div>User catalog:</div>
-          <div>
-            <b>Hint</b>: to see more results typing specific name
-          </div>
+          <div><b>Hint</b>: to see more results typing specific name</div>
           <input
             type="text"
             className="au-input au-input--full"
