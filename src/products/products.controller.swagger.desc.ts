be78dd030ed024730424c3b30f3897cef880d9dd
Tamir Gershberg <47638346+tamirGer@users.noreply.github.com>
2023-02-11 13:28:41 +0200

    Update src/products/products.controller.swagger.desc.ts
    Co-authored-by: Artem Derevnjuk <artem.derevnjuk@brightsec.com>

--- src/products/products.controller.swagger.desc.ts
+++ src/products/products.controller.swagger.desc.ts
@@ -2,4 +2,4 @@ export const SWAGGER_DESC_GET_PRODUCTS = `Returns all products`;
 
 export const SWAGGER_DESC_GET_LATEST_PRODUCTS = `Returns 3 latest products`;
 
-export const SWAGGER_DESC_GET_VIEW_PRODUCT = `Updates the product's views_count according to product name provided in header 'product-name' and returns the query result`;
+export const SWAGGER_DESC_GET_VIEW_PRODUCT = `Updates the product's 'viewsCount' according to product name provided in the header 'x-product-name' and returns the query result.`;
