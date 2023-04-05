6beb7116837f26ce7f3bdae637cb6aeb6c80726c
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2023-04-05 13:17:39 +0400

    fix(auth): avoid using native request in resolvers

--- src/testimonials/testimonials.resolver.ts
+++ src/testimonials/testimonials.resolver.ts
@@ -1,9 +1,4 @@
-import {
-  InternalServerErrorException,
-  Logger,
-  UseGuards,
-} from '@nestjs/common';
-import { AuthGuard } from '../auth/auth.guard';
+import { Logger, UseGuards } from '@nestjs/common';
 import { JwtProcessorType } from '../auth/auth.service';
 import { JwtType } from '../auth/jwt/jwt.type.decorator';
 import { Query, Mutation, Resolver, Args, Int } from '@nestjs/graphql';
@@ -16,32 +11,33 @@ import {
   API_DESC_GET_TESTIMONIALS,
   API_DESC_GET_TESTIMONIALS_ON_SQL_QUERY,
 } from './testimonials.controller.api.desc';
+import { AuthGuard } from '../auth/auth.guard';
 
-@Resolver((of) => Testimonial)
+@Resolver(() => Testimonial)
 export class TestimonialsResolver {
   private readonly logger = new Logger(TestimonialsResolver.name);
 
   constructor(private readonly testimonialsService: TestimonialsService) {}
 
-  @Query((returns) => [Testimonial], {
+  @Query(() => [Testimonial], {
     description: API_DESC_GET_TESTIMONIALS,
   })
-  async allTestimonials(): Promise<Testimonial[]> {
+  public async allTestimonials(): Promise<Testimonial[]> {
     this.logger.debug('Get all testimonials.');
     return (await this.testimonialsService.findAll()).map<TestimonialDto>(
       TestimonialDto.covertToApi,
     );
   }
 
-  @Query((returns) => Int, {
+  @Query(() => Int, {
     description: API_DESC_GET_TESTIMONIALS_ON_SQL_QUERY,
   })
-  async testimonialsCount(@Args('query') query: string): Promise<number> {
+  public testimonialsCount(@Args('query') query: string): Promise<number> {
     this.logger.debug('Get count of testimonials.');
-    return await this.testimonialsService.count(query);
+    return this.testimonialsService.count(query);
   }
 
-  @Mutation((returns) => Testimonial, {
+  @Mutation(() => Testimonial, {
     description: API_DESC_CREATE_TESTIMONIAL,
   })
   @UseGuards(AuthGuard)
@@ -50,16 +46,12 @@ export class TestimonialsResolver {
     @Args('testimonialRequest') testimonialRequest: CreateTestimonialRequestGQL,
   ) {
     this.logger.debug('Create testimonial.');
-    try {
-      return TestimonialDto.covertToApi(
-        await this.testimonialsService.createTestimonial(
-          testimonialRequest.message,
-          testimonialRequest.name,
-          testimonialRequest.title,
-        ),
-      );
-    } catch {
-      throw InternalServerErrorException;
-    }
+    return TestimonialDto.covertToApi(
+      await this.testimonialsService.createTestimonial(
+        testimonialRequest.message,
+        testimonialRequest.name,
+        testimonialRequest.title,
+      ),
+    );
   }
 }
