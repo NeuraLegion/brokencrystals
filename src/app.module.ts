82ffde653fa645347ee1c566c48573605e10b245
Tamir Gershberg <tamir.ger@gmail.com>
2023-04-21 14:12:06 +0300

    add getCommandResult gql query with OSI

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,15 @@ import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { TestimonialsModule } from './testimonials/testimonials.module';
 import { ProductsModule } from './products/products.module';
 import { OrmModule } from './orm/orm.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpClientService } from './httpclient/httpclient.service';
 import { HttpClientModule as HttpClientModule } from './httpclient/httpclient.module';
 import { TraceMiddleware } from './components/trace.middleware';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
@@ -34,7 +37,13 @@ import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
     }),
   ],
   controllers: [AppController],
-  providers: [HttpClientService],
+  providers: [
+    HttpClientService,
+    AppService,
+    UsersService,
+    ConfigService,
+    AppResolver,
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
