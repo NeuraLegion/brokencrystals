fd63d2280113d748d94dd79b917cb5f93db2efcc
Tamir Gershberg <47638346+tamirGer@users.noreply.github.com>
2023-04-28 19:23:03 +0300

    feat(osi): add `getCommandResult` query with OSI (#261)
    Add a `getCommandResult` query which executes the provided command
    and returns its result, vulnerable to OSI.

--- src/app.controller.swagger.desc.ts
+++ src/app.controller.swagger.desc.ts
@@ -1,11 +1,11 @@
-export const SWAGGER_DESC_RENDER_REQUEST = `Template for rendering by doT. Expects plain text as request body`;
+export const API_DESC_RENDER_REQUEST = `Template for rendering by doT. Expects plain text as request body`;
 
-export const SWAGGER_DESC_REDIRECT_REQUEST = `Redirects the user to the provided url`;
+export const API_DESC_REDIRECT_REQUEST = `Redirects the user to the provided url`;
 
-export const SWAGGER_DESC_XML_METADATA = `Receives client's metadata in XML format. Returns the passed XML`;
+export const API_DESC_XML_METADATA = `Receives client's metadata in XML format. Returns the passed XML`;
 
-export const SWAGGER_DESC_OPTIONS_REQUEST = `Returns the list of supported operations`;
+export const API_DESC_OPTIONS_REQUEST = `Returns the list of supported operations`;
 
-export const SWAGGER_DESC_LAUNCH_COMMAND = `Launches system command on server`;
+export const API_DESC_LAUNCH_COMMAND = `Launches system command on server`;
 
-export const SWAGGER_DESC_CONFIG_SERVER = `Returns server configuration to the client`;
+export const API_DESC_CONFIG_SERVER = `Returns server configuration to the client`;
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   Header,
   HttpException,
+  InternalServerErrorException,
   Logger,
   Options,
   Param,
@@ -15,7 +16,6 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import {
   ApiBody,
   ApiConsumes,
@@ -27,24 +27,21 @@ import {
   ApiProduces,
   ApiTags,
 } from '@nestjs/swagger';
-import { spawn } from 'child_process';
 import * as dotT from 'dot';
 import { parseXml } from 'libxmljs';
 import { AppConfig } from './app.config.api';
-import { AppModuleConfigProperties } from './app.module.config.properties';
-import { OrmModuleConfigProperties } from './orm/orm.module.config.properties';
 import {
-  SWAGGER_DESC_CONFIG_SERVER,
-  SWAGGER_DESC_LAUNCH_COMMAND,
-  SWAGGER_DESC_OPTIONS_REQUEST,
-  SWAGGER_DESC_REDIRECT_REQUEST,
-  SWAGGER_DESC_RENDER_REQUEST,
-  SWAGGER_DESC_XML_METADATA,
+  API_DESC_CONFIG_SERVER,
+  API_DESC_LAUNCH_COMMAND,
+  API_DESC_OPTIONS_REQUEST,
+  API_DESC_REDIRECT_REQUEST,
+  API_DESC_RENDER_REQUEST,
+  API_DESC_XML_METADATA,
 } from './app.controller.swagger.desc';
-import { UsersService } from './users/users.service';
 import { AuthGuard } from './auth/auth.guard';
 import { JwtType } from './auth/jwt/jwt.type.decorator';
 import { JwtProcessorType } from './auth/auth.service';
+import { AppService } from './app.service';
 import { BASIC_USER_INFO, UserDto } from './users/api/UserDto';
 import { SWAGGER_DESC_FIND_USER } from './users/users.controller.swagger.desc';
 
@@ -53,16 +50,13 @@ import { SWAGGER_DESC_FIND_USER } from './users/users.controller.swagger.desc';
 export class AppController {
   private readonly logger = new Logger(AppController.name);
 
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly userService: UsersService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Post('render')
   @ApiProduces('text/plain')
   @ApiConsumes('text/plain')
   @ApiOperation({
-    description: SWAGGER_DESC_RENDER_REQUEST,
+    description: API_DESC_RENDER_REQUEST,
   })
   @ApiBody({ description: 'Write your text here' })
   @ApiCreatedResponse({
@@ -79,7 +73,7 @@ export class AppController {
 
   @Get('goto')
   @ApiOperation({
-    description: SWAGGER_DESC_REDIRECT_REQUEST,
+    description: API_DESC_REDIRECT_REQUEST,
   })
   @ApiOkResponse({
     description: 'Redirected',
@@ -91,7 +85,7 @@ export class AppController {
 
   @Post('metadata')
   @ApiOperation({
-    description: SWAGGER_DESC_XML_METADATA,
+    description: API_DESC_XML_METADATA,
   })
   @ApiInternalServerErrorResponse({
     description: 'Invalid data',
@@ -117,7 +111,7 @@ export class AppController {
 
   @Options()
   @ApiOperation({
-    description: SWAGGER_DESC_OPTIONS_REQUEST,
+    description: API_DESC_OPTIONS_REQUEST,
   })
   @Header('allow', 'OPTIONS, GET, HEAD, POST')
   async getTestOptions(): Promise<void> {
@@ -126,7 +120,7 @@ export class AppController {
 
   @Get('spawn')
   @ApiOperation({
-    description: SWAGGER_DESC_LAUNCH_COMMAND,
+    description: API_DESC_LAUNCH_COMMAND,
   })
   @ApiOkResponse({
     type: String,
@@ -137,38 +131,18 @@ export class AppController {
       properties: { location: { type: 'string' } },
     },
   })
-  async launchCommand(@Query('command') command: string): Promise<string> {
+  async getCommandResult(@Query('command') command: string): Promise<string> {
     this.logger.debug(`launch ${command} command`);
-
-    return new Promise((res, rej) => {
-      try {
-        const [exec, ...args] = command.split(' ');
-        const ps = spawn(exec, args);
-
-        ps.stdout.on('data', (data: Buffer) => {
-          this.logger.debug(`stdout: ${data}`);
-          res(data.toString('ascii'));
-        });
-
-        ps.stderr.on('data', (data: Buffer) => {
-          this.logger.debug(`stderr: ${data}`);
-          res(data.toString('ascii'));
-        });
-
-        ps.on('error', (err) => rej(err.message));
-
-        ps.on('close', (code) =>
-          this.logger.debug(`child process exited with code ${code}`),
-        );
-      } catch (err) {
-        rej(err.message);
-      }
-    });
+    try {
+      return await this.appService.launchCommand(command);
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
   }
 
   @Get('/config')
   @ApiOperation({
-    description: SWAGGER_DESC_CONFIG_SERVER,
+    description: API_DESC_CONFIG_SERVER,
   })
   @ApiOkResponse({
     type: AppConfig,
@@ -176,30 +150,8 @@ export class AppController {
   })
   getConfig(): AppConfig {
     this.logger.debug('Called getConfig');
-    const dbSchema = this.configService.get<string>(
-      OrmModuleConfigProperties.ENV_DATABASE_SCHEMA,
-    );
-    const dbHost = this.configService.get<string>(
-      OrmModuleConfigProperties.ENV_DATABASE_HOST,
-    );
-    const dbPort = this.configService.get<string>(
-      OrmModuleConfigProperties.ENV_DATABASE_PORT,
-    );
-    const dbUser = this.configService.get<string>(
-      OrmModuleConfigProperties.ENV_DATABASE_USER,
-    );
-    const dbPwd = this.configService.get<string>(
-      OrmModuleConfigProperties.ENV_DATABASE_PASSWORD,
-    );
-    return {
-      awsBucket: this.configService.get<string>(
-        AppModuleConfigProperties.ENV_AWS_BUCKET,
-      ),
-      sql: `postgres://${dbUser}:${dbPwd}@${dbHost}:${dbPort}/${dbSchema} `,
-      googlemaps: this.configService.get<string>(
-        AppModuleConfigProperties.ENV_GOOGLE_MAPS,
-      ),
-    };
+    const config = this.appService.getConfig();
+    return config;
   }
 
   @Get('/v1/userinfo/:email')
@@ -224,8 +176,7 @@ export class AppController {
   })
   async getUserInfo(@Param('email') email: string): Promise<UserDto> {
     try {
-      this.logger.debug(`Find a user by email: ${email}`);
-      return new UserDto(await this.userService.findByEmail(email));
+      return await this.appService.getUserInfo(email);
     } catch (err) {
       throw new HttpException(err.message, err.status);
     }
@@ -255,8 +206,7 @@ export class AppController {
   })
   async getUserInfoV2(@Param('email') email: string): Promise<UserDto> {
     try {
-      this.logger.debug(`Find a user by email: ${email}`);
-      return new UserDto(await this.userService.findByEmail(email));
+      return await this.appService.getUserInfo(email);
     } catch (err) {
       throw new HttpException(err.message, err.status);
     }
