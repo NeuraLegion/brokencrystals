2b7d3dc7be7325291fe6589415fb336b41670007
Maxim Ashin <maximashin369@gmail.com>
2023-12-26 02:34:02 +0200

    WIP: Adding XPATH support

--- src/testimonials/testimonials.controller.api.desc.ts
+++ src/testimonials/testimonials.controller.api.desc.ts
@@ -3,3 +3,5 @@ export const API_DESC_CREATE_TESTIMONIAL = `Creates testimonial`;
 export const API_DESC_GET_TESTIMONIALS = `Returns all testimonials`;
 
 export const API_DESC_GET_TESTIMONIALS_ON_SQL_QUERY = `Returns count of all testimonials based on provided sql query`;
+
+export const API_DESC_GET_TESTIMONIAL_AUTHORS = `Returns testimonial's authors by query, based on "XPATH-ing" an XML file`;
