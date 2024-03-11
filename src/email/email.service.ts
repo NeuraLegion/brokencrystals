984d36b727284f6a9e0dae95a4a05af4ba2b5d1d
Maxim Ashin <maximashin369@gmail.com>
2024-03-11 02:36:07 +0200

    WIP: Sending custom mails

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,6 +1,43 @@
 import { Injectable, Logger } from '@nestjs/common';
+const nodemailer = require('nodemailer');
 
 @Injectable()
 export class EmailService {
   private readonly logger = new Logger(EmailService.name);
+
+  private readonly smtpServerDetails = {
+    host: '127.0.0.1',
+    port: 1025,
+    secure: false,
+    auth: {
+      user: 'mailcatcher',
+      pass: 'mailcatcher',
+    },
+  };
+
+  private readonly transporter = nodemailer.createTransport(
+    this.smtpServerDetails,
+  );
+
+  async sendMail(
+    from: string,
+    to: string,
+    subject: string,
+    body: string,
+  ): Promise<boolean> {
+    this.logger.debug(`Sending mail from "${from}" to "${to}"`);
+    this.logger.debug(
+      `Mail subject is (shortened): "${subject.substring(0, 16)}"`,
+    );
+    this.logger.debug(`Mail body is (shortened): "${body.substring(0, 16)}"`);
+
+    let result = await this.transporter.sendMail({
+      from: from,
+      to: to,
+      subject: subject,
+      html: body,
+    });
+
+    return Boolean(result);
+  }
 }
