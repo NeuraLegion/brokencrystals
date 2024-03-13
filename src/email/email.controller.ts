b236f9a0be176215cc89456ed50c435d4f3c162b
Maxim Ashin <maximashin369@gmail.com>
2024-03-13 03:59:00 +0200

    WIP: Sending mail and debugging some fe problems

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,11 +1,12 @@
+import { Controller, Get, Logger, Query } from '@nestjs/common';
 import {
-  Controller,
-  Logger,
-} from '@nestjs/common';
-import {
+  ApiHeader,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
   ApiTags,
 } from '@nestjs/swagger';
-import {EmailService} from './email.service'
+import { EmailService } from './email.service';
 
 @Controller('/api/email')
 @ApiTags('Email controller')
@@ -13,4 +14,49 @@ export class EmailController {
   private readonly logger = new Logger(EmailController.name);
 
   constructor(private emailService: EmailService) {}
+
+  @Get()
+  @ApiQuery({
+    name: 'to',
+    example: 'username@gmail.com',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'subject',
+    example: 'Help Request',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'content',
+    example: 'I would like to request help regarding the matter of..',
+    required: true,
+  })
+  @ApiHeader({ name: 'accept', example: 'image/jpg', required: true })
+  @ApiOkResponse({
+    description: 'File read successfully',
+  })
+  // @ApiInternalServerErrorResponse({
+  //   schema: {
+  //     type: 'object',
+  //     properties: {
+  //       error: { type: 'string' },
+  //       location: { type: 'string' },
+  //     },
+  //   },
+  // })
+
+  // @ApiOperation({
+  //   description: SWAGGER_DESC_READ_FILE,
+  // })
+  async sendMail(
+    @Query('to') to: string,
+    @Query('subject') subject: string,
+    @Query('content') content: string,
+    // @Res({ passthrough: true }) res: FastifyReply,
+    // @Headers('accept') acceptHeader: string,
+  ) {
+    // TODO: Remove this in prod, this is a debug email
+    let from = 'testMail@brokencrystals.com';
+    await this.emailService.sendMail(from, to, subject, content);
+  }
 }
