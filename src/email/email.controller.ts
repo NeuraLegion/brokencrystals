afbb6e43bb4b936742a64d599cf889cfe162148e
Maxim Ashin <maximashin369@gmail.com>
2024-03-18 03:45:33 +0200

    WIP: BE PP

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,5 +1,3 @@
-import splitUriIntoParamsPPVulnerable from '../../client/src/utils/url';
-
 import { FastifyReply } from 'fastify';
 import {
   Controller,
@@ -12,7 +10,6 @@ import {
   Res,
 } from '@nestjs/common';
 import {
-  ApiOkResponse,
   ApiOperation,
   ApiQuery,
   ApiTags,
@@ -75,7 +72,7 @@ export class EmailController {
     // A server side prototype pollution can be found in the `name` param
     // You can create a fake `status` variable and return a tempered response
     const formattedUrlWithName = `?name=${name}`;
-    splitUriIntoParamsPPVulnerable(formattedUrlWithName);
+    await this.splitUriIntoParamsPPVulnerable(formattedUrlWithName);
 
     let responseJson = {
       status: HttpStatus.OK,
@@ -107,5 +104,78 @@ export class EmailController {
   })
   async deleteEmails() {
     return await this.emailService.deleteEmails();
+  }
+
+  async splitUriIntoParamsPPVulnerable(params, coerce=null) {
+    if (params.charAt(0) === '?') {
+      params = params.substring(1);
+    }
+
+    var obj = {},
+      coerce_types = { true: !0, false: !1, null: null };
+
+    if (!params) {
+      return obj;
+    }
+
+    params
+      .replace(/\+/g, ' ')
+      .split('&')
+      .forEach(function (v) {
+        var param = v.split('='),
+          key = decodeURIComponent(param[0]),
+          val,
+          cur = obj,
+          i = 0,
+          keys = key.split(']['),
+          keys_last = keys.length - 1;
+
+        if (/\[/.test(keys[0]) && /\]$/.test(keys[keys_last])) {
+          keys[keys_last] = keys[keys_last].replace(/\]$/, '');
+          keys = keys.shift().split('[').concat(keys);
+          keys_last = keys.length - 1;
+        } else {
+          keys_last = 0;
+        }
+
+        if (param.length === 2) {
+          val = decodeURIComponent(param[1]);
+
+          if (coerce) {
+            val =
+              val && !isNaN(val) && +val + '' === val
+                ? +val // number
+                : val === 'undefined'
+                ? undefined // undefined
+                : coerce_types[val] !== undefined
+                ? coerce_types[val] // true, false, null
+                : val; // string
+          }
+
+          if (keys_last) {
+            for (; i <= keys_last; i++) {
+              //@ts-ignore
+              key = keys[i] === '' ? cur.length : keys[i];
+              cur = cur[key] =
+                i < keys_last
+                  ? //@ts-ignore
+                    cur[key] || (keys[i + 1] && isNaN(keys[i + 1]) ? {} : [])
+                  : val;
+            }
+          } else {
+            if (Object.prototype.toString.call(obj[key]) === '[object Array]') {
+              obj[key].push(val);
+            } else if ({}.hasOwnProperty.call(obj, key)) {
+              obj[key] = [obj[key], val];
+            } else {
+              obj[key] = val;
+            }
+          }
+        } else if (key) {
+          obj[key] = coerce ? undefined : '';
+        }
+      });
+
+    return obj;
   }
 }
