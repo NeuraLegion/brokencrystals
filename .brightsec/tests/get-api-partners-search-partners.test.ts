a12e001c5bada678aa7bbcccc6a348ebec6e6db3
Viachaslau <slava@tssbox.com>
2026-01-22 16:45:51 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

--- .brightsec/tests/get-api-partners-search-partners.test.ts
+++ .brightsec/tests/get-api-partners-search-partners.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { test, before, after } from 'node:test';
 import { SecRunner } from '@sectester/runner';
 import { AttackParamLocation, HttpMethod } from '@sectester/scan';
@@ -19,22 +18,56 @@ before(async () => {
 
 after(() => runner.clear());
 
-test(
-  'GET /api/partners/searchPartners',
-  { signal: AbortSignal.timeout(timeout) },
-  async () => {
-    await runner
-      .createScan({
-        tests: ['xpathi', 'xss', 'full_path_disclosure'],
-        attackParamLocations: [AttackParamLocation.QUERY]
-      })
-      .setFailFast(false)
-      .timeout(timeout)
-      .run({
-        method: HttpMethod.GET,
-        url: `${baseUrl}/api/partners/searchPartners?keyword=Walter`,
-        headers: { 'Content-Type': 'text/xml' },
-        auth: process.env.BRIGHT_AUTH_ID
-      });
-  }
-);
+test('GET /api/partners/searchPartners', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['xpathi', 'xss', 'csrf'],
+      attackParamLocations: [AttackParamLocation.QUERY, AttackParamLocation.HEADER],
+      starMetadata: {
+        "code_source": "NeuraLegion/brokencrystals:bench/fix_workflow_tc01",
+        "databases": ["PostgreSQL"],
+        "user_roles": {
+          "roles": [
+            "default-roles-brokencrystals",
+            "offline_access",
+            "uma_authorization",
+            "query-users",
+            "view-authorization",
+            "create-client",
+            "realm-admin",
+            "manage-users",
+            "manage-authorization",
+            "query-realms",
+            "view-events",
+            "manage-clients",
+            "view-realm",
+            "manage-realm",
+            "impersonation",
+            "query-clients",
+            "query-groups",
+            "manage-events",
+            "view-clients",
+            "view-identity-providers",
+            "view-users",
+            "manage-identity-providers",
+            "read-token",
+            "view-profile",
+            "manage-account-links",
+            "manage-account",
+            "manage-consent",
+            "view-applications",
+            "view-consent",
+            "delete-account"
+          ]
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/api/partners/searchPartners?keyword=Walter`,
+      headers: { 'Content-Type': 'text/xml' }
+    });
+});
