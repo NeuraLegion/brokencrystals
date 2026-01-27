de4eb9cc6a28f93d53d03ab2c6a56a5529fcf45e
Viachaslau <slava@tssbox.com>
2026-01-27 20:54:22 +0400

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/get-api-partners-search-partners.test.ts
+++ .brightsec/tests/get-api-partners-search-partners.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('GET /api/partners/searchPartners', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['xpathi', 'xss', 'full_path_disclosure'],
+      tests: ['xpathi'],
       attackParamLocations: [AttackParamLocation.QUERY],
       starMetadata: {
         code_source: "NeuraLegion/brokencrystals:fixer/fix_workflow_tc01",
