9886ac4434659abfde22afda6d5401698d67e4f6
Maksym Hysko <hysko.mv@gmail.com>
2026-03-06 16:33:23 +0200

    chore: concurrency test

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -30,7 +30,7 @@ export class ChatController {
       return await this.chatService.query(messages);
     } catch (err) {
       throw new HttpException(
-        `Chat API response error: ${err}`,
+        `Chat API response err: ${err}`,
         HttpStatus.INTERNAL_SERVER_ERROR
       );
     }
