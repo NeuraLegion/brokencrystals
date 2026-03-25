ece12dc56278828e72793c87e3e2ec1a341bf8fd
Viachaslau <slava@tssbox.com>
2026-03-25 10:25:27 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/get-api-partners-partner-login.test.ts
@@ -0,0 +1,71 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('GET /api/partners/partnerLogin', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['xpathi', 'xss', 'full_path_disclosure'],
+      attackParamLocations: [AttackParamLocation.QUERY],
+      starMetadata: {
+        "code_source": "NeuraLegion/brokencrystals:fixer/fix_workflow_tc01",
+        "databases": ["PostgreSQL"],
+        "user_roles": [
+          "default-roles-brokencrystals",
+          "offline_access",
+          "uma_authorization",
+          "query-users",
+          "view-authorization",
+          "create-client",
+          "realm-admin",
+          "manage-users",
+          "manage-authorization",
+          "query-realms",
+          "view-events",
+          "manage-clients",
+          "view-realm",
+          "manage-realm",
+          "impersonation",
+          "query-clients",
+          "query-groups",
+          "manage-events",
+          "view-clients",
+          "view-identity-providers",
+          "view-users",
+          "manage-identity-providers",
+          "read-token",
+          "view-profile",
+          "manage-account-links",
+          "manage-account",
+          "manage-consent",
+          "view-applications",
+          "view-consent",
+          "delete-account"
+        ]
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/api/partners/partnerLogin?username=walter100&password=Heisenberg123`,
+      headers: { 'Content-Type': 'text/xml' }
+    });
+});
